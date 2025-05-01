66e63e4ddc270608ad3f14e351130a180d4aa194
Drew-bautista <142853218+Drew-bautista@users.noreply.github.com>
2025-05-01 21:53:55 +0800

    new

---
+++ database/databaseConnector.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const ConnectDatabase = () => {
+  const uri = process.env.MONGODB_URI || process.env.CONNECTION_URL;
+  if (!uri) {
+    throw new Error('No MongoDB connection string found in environment variables!');
+  }
+  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+};
+
+module.exports = { ConnectDatabase };
